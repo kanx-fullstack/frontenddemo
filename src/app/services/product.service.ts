33c2cd837e1908f5bd03736f5b8423f203f0f693
kanchan <kanchan.vdoit@gmail.com>
2025-06-05 17:35:04 +0530

    app

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,6 +8,8 @@ import { Product } from '../models/product';
 })
 export class ProductService {
 
+  
+
   private apiUrl = 'http://localhost:3000/api/products';
 
   constructor(private http: HttpClient) { }
